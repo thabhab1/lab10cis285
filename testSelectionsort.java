d3bb7d2926b706803ea1cdf067835b4368fa1228
thabet <thabhab@umich.edu>
2021-12-09 13:31:32 -0500

    tom commit

--- testSelectionsort.java
+++ testSelectionsort.java
@@ -18,6 +18,7 @@ testDuplicates();
     public testSelectionSort() {
     }
 
+    SelectionSort sel = new SelectionSort();
     public void testPositive(){
         int[] arr = new int[5];
         arr[0] = 8;
@@ -25,6 +26,8 @@ testDuplicates();
         arr[2] = 7;
         arr[3] = 10;
         arr[4] = 2;
+
+        arr = sel.basicSelectionSort(arr);
 
         int[] Sortedarr = new int[5];
         Sortedarr[0] = 2;
@@ -34,9 +37,35 @@ testDuplicates();
         Sortedarr[4] = 10;
 
         /** add tests to check for this unit test **/
+        assertArrayEquals(arr, Sortedarr);
+
+//        for(int i = 0; i<5; i++) {
+//        	System.out.println(i);
+//
+//
+//        }
     }
 
     public void testNegative(){
+
+    	int[] arr = new int[5];
+        arr[0] = -8;
+        arr[1] = -9;
+        arr[2] = -7;
+        arr[3] = -10;
+        arr[4] = -2;
+
+        arr = sel.basicSelectionSort(arr);
+
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = -10;
+        Sortedarr[1] = -9;
+        Sortedarr[2] = -8;
+        Sortedarr[3] = -7;
+        Sortedarr[4] = -2;
+
+        /** add tests to check for this unit test **/
+        assertArrayEquals(arr, Sortedarr);
 
         /** Test data contains negative values only **/
     }
